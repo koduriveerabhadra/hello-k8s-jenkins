9f67d2243e39281649ca0749dbd6d38a2eb7a48e
koduriveerabhadra <koduri.veerabhadra765@gmail.com>
2026-01-06 17:16:23 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,15 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "hello-eks"
+  cluster_version = "1.29"
 
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+}
